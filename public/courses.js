e3d6d63c136212b67025e37db7465e5453d0f9ae
Alex Gyori <alexandergyori@gmail.com>
2016-11-22 02:43:57 -0500

    Basic node infrastructure, load courses from DB, add test Course to DB

---
+++ public/courses.js
@@ -0,0 +1,59 @@
+﻿var viewModel = (function () {
+    var courseList = ko.observableArray();
+
+    var exports = {};
+    exports.courseList = courseList;
+    return exports;
+}());
+
+ko.applyBindings(viewModel);
+
+var populateCourseList = function (courses) {
+    viewModel.courseList.removeAll();
+    courses.forEach(function (val) {
+        viewModel.courseList.push(val);
+    });
+};
+
+var loadCourseList = function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "getAllCourses", true);
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) { // TODO: ADD LOADING GIF HERE
+            populateCourseList(JSON.parse(xhttp.responseText));
+        }
+    };
+    xhttp.send();
+};
+
+var addCourse = function (course) {
+    // test code, remove
+    course = {
+        courseCode: "test",
+        title: "test",
+        startDate: null,
+        endDate: null,
+        hasLab: true,
+        isActive: true
+    };
+
+    var xhttp = new XMLHttpRequest();
+    var params =
+        "courseCode=" + (course.courseCode || '') + "&" + 
+        "title=" + (course.title || '') + "&" + 
+        "startDate=" + (course.startDate || '') + "&" +
+        "endDate=" + (course.endDate || '') + "&" +
+        "hasLab=" + (course.hasLab || '') + "&" +
+        "isActive=" + (course.isActive || '');
+    xhttp.open("POST", "addCourse", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            alert("Successfully added course!");
+        }
+    };
+    console.log("params " + params);
+    xhttp.send(params);
+};
+
+loadCourseList();
---
+++ taManager.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var pgp = require('pg-promise')(/* options */);
+var bodyParser = require('body-parser');
+var db = pgp('postgres://postgres:password@localhost:5432/postgres'); // TODO: abstract out the various requirements in this string
+
+var taManager = express();
+taManager.use(bodyParser.json());       // only need this for JSON-encoded bodies
+taManager.use(bodyParser.urlencoded({   // only need this for URL-encoded bodies
+    extended: true
+}));
+taManager.use(express.static(__dirname + '/public'));
+
+// Now we can use
+// POST: name=foo&color=red            <-- URL encoding
+// and
+// POST: {"name":"foo","color":"red"}  <-- JSON encoding
+// req.body.name for both
+
+// =========================================== COURSES PAGE ===========================================
+taManager.get('/getAllCourses', function (req, res) {
+    db.query('SELECT "CourseId" AS "courseId", "CourseCode" AS "courseCode", "Title" AS "title", "StartDate" AS "startDate", "EndDate" AS "endDate", "HasLab" AS "hasLab", "IsActive" AS "isActive" FROM thesis."Course" ORDER BY "CourseCode" ASC;')
+        .then(function (data) {
+            res.send(data);
+        })
+        .catch(function (error) {
+            console.log('ERROR: ', error);
+        });
+});
+
+taManager.post('/addCourse', function (req, res) {
+    var course = req.body;
+    db.query('INSERT INTO thesis."Course"("CourseCode", "Title", "StartDate", "EndDate", "HasLab", "IsActive") ' +
+        'VALUES ($1, $2, $3, $4, $5, $6);', [course.courseCode, course.title, null, null, course.hasLab, course.isActive])
+        .then(function (data) {
+            res.send();
+        })
+        .catch(function (error) {
+            console.log('ERROR: ', error);
+        });
+});
+
+// =========================================== TEST FUNCTIONS ===========================================
+taManager.get('/', function (req, res) {
+    res.send('Hello World!!');
+});
+
+taManager.get('/airspeed', function (req, res) {
+    res.send('African or European?');
+});
+
+taManager.get('/africanOrEuropean', function (req, res) {
+    if (req.query.birdType === "AFRICAN") {
+        res.send('11');
+    } else {
+        res.send('22');
+    }
+});
+
+taManager.post('/africanOrEuropean', function (req, res) {
+    if (req.body.birdType === "AFRICAN") {
+        res.send('33');
+    } else {
+        res.send('44');
+    }
+});
+
+taManager.get('/testDb', function (req, res) {
+    db.one('SELECT * FROM thesis."Course"')
+        .then(function (data) {
+            console.log('DATA: ', data.value);
+            res.send(data);
+        })
+        .catch(function (error) {
+            console.log('ERROR: ', error);
+        });
+});
+
+// Listen on 8080
+taManager.listen(8080, function () {
+    console.log('Example app listening on port 8080');
+});
